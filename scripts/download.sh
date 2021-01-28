26ef833c18dafd9eff214045c710d0767e28d7b5
clems grisi <clementnet@outlook.com>
2021-01-28 19:12:48 +0100

    fixed data download bashscript

--- scripts/download.sh
+++ scripts/download.sh
@@ -1,3 +1,5 @@
 #!/usr/bin/env bash
-gdown "https://www.researchgate.net/profile/Pekka_Malo/publication/251231364_FinancialPhraseBank-v10/data/0c96051eee4fb1d56e000000/FinancialPhraseBank-v10.zip" --output data/
+wget -O data/FinancialPhraseBank-v10.zip "https://www.researchgate.net/profile/Pekka_Malo/publication/251231364_FinancialPhraseBank-v10/data/0c96051eee4fb1d56e000000/FinancialPhraseBank-v10.zip"
 unzip data/FinancialPhraseBank-v10.zip -d data/
+rm -r data/__MACOSX/
+rm data/FinancialPhraseBank-v10.zip
